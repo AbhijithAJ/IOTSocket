c87513cebcff9d5bcdb28785e46beead71ec49e8
AbhijithAJ <47808835+AbhijithAJ@users.noreply.github.com>
2020-03-19 15:29:17 +0530

    Update IOTClientExample.py

--- IOTClientExample.py
+++ IOTClientExample.py
@@ -48,6 +48,8 @@ while 1: # reconnect if socket is closed
                 handleCmdsFromServer(rcv_data)   # handle your data here
 
     except Exception as n:
-        if (type(s) != "<class 'str'>"):
+        try:
             s.s.close()
+        except:
+            pass
         print(n)
